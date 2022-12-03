79ce8de14ca988f160db6cb6f6b7a2929463d1d4
Francesca-Barcella <pollon.fra@gmail>
2022-12-03 22:29:51 +0100

    fix bug

--- bad_words.php
+++ bad_words.php
@@ -7,7 +7,7 @@ $parola = $_GET['parola'];
 //echo $parola .'<br>';
 
 
-$frase_esplosa = explode (' ', $frase);
+$frase_esplosa = explode(' ', $frase);
 //var_dump($frase_esplosa) .'<br>';
 
 $parola_censurata = str_ireplace($parola, '***', $frase);
@@ -19,15 +19,18 @@ $parola_censurata = str_ireplace($parola, '***', $frase);
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>bad_word</title>
 </head>
+
 <body>
     <p>La frase che hai inserito è lunga <strong> <?php echo strlen($frase); ?> </strong> caratteri ed è la segutente: <br> <strong><?php echo $frase; ?></strong></p>
     <p>La parola che hai scelto di riscrivere è la segutente: <br> <strong><?php echo $parola; ?></strong></p>
     <p>La nuova frase è quindi: <br> <strong><?php echo $parola_censurata  ?></strong></p>
 </body>
+
 </html>
--- index.php
+++ index.php
@@ -1,33 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Form bad word</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
-        integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
+
 <body>
     <div class="container">
         <h1>Bad word</h1>
-            <form action="bad_words.php" method="get">
-                <div class="mb-3">
-                    <label for="frase" class="form-label">frase:</label>
-                    <small id="fraseHelper" class="text-muted">scrivi una frase qui sotto</small>
-                    <input type="text" name="frase" id="frase" class="form-control" placeholder="Scrivi qui ..." aria-describedby="paragrafoHelper">
-                </div>
-                <div class="mb-3">
-                    <label for="parola" class="form-label">Parola:</label>
-                    <small id="parolaHelper" class="text-muted">scegli una parola della tua frase e inseriscila qui sotto</small>
-                    <input type="text" name="parola" id="parola" class="form-control" placeholder="Scrivi qui ..." aria-describedby="parolaHelper">
-                </div>
-                <button type="submit" class="btn btn-primary">Invia</button>
-                <button type="reset" class="btn btn-danger">Cancella</button>
-            </form>
-        </div>
+        <form action="bad_words.php" method="get">
+            <div class="mb-3">
+                <label for="frase" class="form-label">frase:</label>
+                <small id="fraseHelper" class="text-muted">scrivi una frase qui sotto</small>
+                <input type="text" name="frase" id="frase" class="form-control" placeholder="Scrivi qui ..." aria-describedby="paragrafoHelper">
+            </div>
+            <div class="mb-3">
+                <label for="parola" class="form-label">Parola:</label>
+                <small id="parolaHelper" class="text-muted">scegli una parola della tua frase e inseriscila qui sotto</small>
+                <input type="text" name="parola" id="parola" class="form-control" placeholder="Scrivi qui ..." aria-describedby="parolaHelper">
+            </div>
+            <button type="submit" class="btn btn-primary">Invia</button>
+            <button type="reset" class="btn btn-danger">Cancella</button>
+        </form>
+    </div>
 </body>
+
 </html>
